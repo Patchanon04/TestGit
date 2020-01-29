6d1221f55bc5219b962e031dc1a8728270831012
Patchanon04 <60419702+Patchanon04@users.noreply.github.com>
2020-01-29 20:30:05 +0700

    Create exercise4_Patchanon_W.py

---
+++ exercise4_Patchanon_W.py
@@ -0,0 +1,11 @@
+ENG = 49.5
+GB = 68
+COM = 87
+PRO = 36
+
+
+print("--- Your Score ---")
+print("Foundation English : ",ENG)
+print("General Business :  : ",GB)
+print("Introduction to Computer Systems : ",COM)
+print("Computer Programming : ",PRO)
