922cea20d7b3bfb9cda9d88a7209176f37269497
Patchanon04 <60419702+Patchanon04@users.noreply.github.com>
2020-01-30 19:24:01 +0700

    Create exercise5_1_Patchanon_W.py

---
+++ exercise5_1_Patchanon_W.py
@@ -0,0 +1,10 @@
+num1 = int(input("number1 :"))
+num2 = int(input("number2 :"))
+result1 = num1+num2
+result2 = num1-num2
+result3 = num1*num2
+result4 = num1/num2
+print(result1)
+print(result2)
+print(result3)
+print(result4)
